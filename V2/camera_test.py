bfaa49468a4c338d75334cbd27c130d8dfdeb8b5
Max <128126712+Dropdead072@users.noreply.github.com>
2024-07-10 11:41:31 +0300

    Add files via upload

---
+++ V2/camera_test.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+_, frame = cap.read()
+
+print(frame.shape)
